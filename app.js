004199dfb8129d48881d7a212763079390339817
Logan <55511399+ThyLAW@users.noreply.github.com>
2021-09-20 20:48:37 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+// app
+
+// declare values
+var annualUsageRate = 240
+var orderingCost = 5
+var holdingCostPercentage = 0.3
+var costPerUnit = 7.00
+
+// compute function
+
+function determineEOQ(
+  U,
+  O,
+  h,
+  C
+) {
+  return Math.sqrt(
+    2 * U * O / (h * C));
+}
+
+//call function
+
+EOQ = determineEOQ(annualUsageRate, orderingCost, holdingCostPercentage, costPerUnit);
+
+// render out to console
+console.log(EOQ);
+
